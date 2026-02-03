95eef209205aba917a97e1a7ae64cf6696a416f4
BishamSinchiury <bishamsinchiury1116@gmail.com>
2026-02-03 14:35:25 +0545

    changes

--- sms/settings.py
+++ sms/settings.py
@@ -154,10 +154,3 @@ STATIC_URL = 'static/'
 
 # Email Backend for Development
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bishamsinchiury1116@gmail.com'
-EMAIL_HOST_PASSWORD = 'hzia piom ikyj jkrk'  # App password without extra quotes
-DEFAULT_FROM_EMAIL = 'bishamsinchiury1116@gmail.com'
